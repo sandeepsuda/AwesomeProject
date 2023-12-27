c35006c514fd0fa604054d077a6e01fd00cfd777
sandeepsuda <sandeep.ssp93@gmail.com>
2023-12-27 17:15:48 +0530

    fixed linter issues and test files

--- App.tsx
+++ App.tsx
@@ -12,61 +12,32 @@ import {
   createNavigationContainerRef,
 } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
-} from '@apollo/client';
+
 // import {Provider} from 'react-redux';
 // import store from './store';
 // import MoviesList from './MoviesList';
 // import Counter from './Counter';
 // import ContainerComp from './ContainerComp';
 
-const client = new ApolloClient({
-  uri: 'https://flyby-router-demo.herokuapp.com/',
-  cache: new InMemoryCache(),
-});
-
-const GET_LOCATIONS = gql`
-  query GetLocations {
-    locations {
-      id
-      name
-      description
-      photo
-    }
-  }
-`;
-
 const Stack = createNativeStackNavigator();
 const navigationRef = createNavigationContainerRef();
 
 const HomeScreen = () => {
-  // const navigation = useNavigation();
-  const {loading, data} = useQuery(GET_LOCATIONS);
-  console.log(data);
-
-  if (!loading) {
-    return (
-      <View testID="container" style={{flex: 1, justifyContent: 'center'}}>
-        <FlatList
-          data={Array.from(Array(200).keys())}
-          keyExtractor={(item, index) => String(index)}
-          renderItem={({item, index}) => (
-            <View
-              key={index}
-              style={{width: '100%', height: 40, justifyContent: 'center'}}>
-              <Text> {item}</Text>
-            </View>
-          )}
-        />
-      </View>
-    );
-  }
-  return <View testID="container" />;
+  return (
+    <View testID="container" style={{flex: 1, justifyContent: 'center'}}>
+      <FlatList
+        data={Array.from(Array(200).keys())}
+        keyExtractor={(item, index) => String(index)}
+        renderItem={({item, index}) => (
+          <View
+            key={index}
+            style={{width: '100%', height: 40, justifyContent: 'center'}}>
+            <Text> {item}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
 };
 
 const SettingsScreen = () => {
@@ -94,14 +65,12 @@ const App = () => {
   });
 
   return (
-    <ApolloProvider client={client}>
-      <NavigationContainer ref={navigationRef}>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Settings" component={SettingsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ApolloProvider>
+    <NavigationContainer ref={navigationRef}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
     // <ApolloProvider client={client}>
     //   <Provider store={store}>
     //     {/* <Counter /> */}
